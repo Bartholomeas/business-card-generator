ccbea4c3736b18ecaee0e58e4ca4aa0e28a0646d
Bartholomeas <69473527+Bartholomeas@users.noreply.github.com>
2024-01-23 23:39:58 +0100

    [Feat] init testing (#20)
    * [Chore]: minor improvements
    
    * [Chore]: textarea input tests
    
    ---------
    
    Co-authored-by: Bartosz Stefaniak <bartosz.stefaniak@cetuspro.com>

---
+++ src/components/common/form/__tests__/input.test.tsx
@@ -0,0 +1,77 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "../form";
+import { Input } from "../input";
+
+const LABEL_TEXT = "Name";
+const DESCRIPTION_TEXT = "Enter your name";
+
+const testSchema = z.object({
+  test: z.string().min(3, "testMsg"),
+});
+
+interface ComponentTestProps {
+  handleSubmit: () => void;
+  type?: "text" | "password";
+}
+
+const ComponentTest = ({ handleSubmit, type = "text" }: ComponentTestProps) => {
+  const methods = useForm({
+    defaultValues: {
+      test: "",
+    },
+    resolver: zodResolver(testSchema),
+  });
+
+  return (
+    <Form {...methods}>
+      <form onSubmit={methods.handleSubmit(handleSubmit)}>
+        <Input
+          name="test"
+          label={LABEL_TEXT}
+          placeholder={LABEL_TEXT}
+          description={DESCRIPTION_TEXT}
+          type={type}
+        />
+
+        <button type="submit">submit</button>
+      </form>
+    </Form>
+  );
+};
+
+describe("Input", () => {
+  it("should render the input field with label and description", () => {
+    const handleSubmit = jest.fn();
+
+    render(<ComponentTest handleSubmit={handleSubmit} />);
+
+    const labelElement = screen.queryByLabelText(LABEL_TEXT);
+    const descriptionElement = screen.getByText(DESCRIPTION_TEXT);
+    const inputElement = screen.getByRole("textbox");
+
+    expect(labelElement).toBeInTheDocument();
+    expect(descriptionElement).toBeInTheDocument();
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  it("should handle input value change", () => {
+    render(<ComponentTest handleSubmit={() => null} />);
+
+    const inputElement = screen.getByLabelText(LABEL_TEXT);
+
+    fireEvent.change(inputElement, { target: { value: "John" } });
+
+    expect(inputElement).toHaveDisplayValue("John");
+  });
+
+  it("should render input with passed type", () => {
+    render(<ComponentTest handleSubmit={() => null} type="password" />);
+
+    const inputElement = screen.getByLabelText(LABEL_TEXT);
+
+    expect(inputElement).toHaveAttribute("type", "password");
+  });
+});
---
+++ src/components/common/form/__tests__/textarea.test.tsx
@@ -0,0 +1,69 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Form } from "../form";
+import { InputTextarea } from "../input-textarea";
+
+const LABEL_TEXT = "Name";
+const DESCRIPTION_TEXT = "Enter your name";
+
+const testSchema = z.object({
+  test: z.string().min(3, "testMsg"),
+});
+
+interface ComponentTestProps {
+  handleSubmit: () => void;
+  type?: "text" | "password";
+}
+
+const ComponentTest = ({ handleSubmit }: ComponentTestProps) => {
+  const methods = useForm({
+    defaultValues: {
+      test: "",
+    },
+    resolver: zodResolver(testSchema),
+  });
+
+  return (
+    <Form {...methods}>
+      <form onSubmit={methods.handleSubmit(handleSubmit)}>
+        <InputTextarea
+          name="test"
+          label={LABEL_TEXT}
+          placeholder={LABEL_TEXT}
+          description={DESCRIPTION_TEXT}
+        />
+
+        <button type="submit">submit</button>
+      </form>
+    </Form>
+  );
+};
+
+describe("textArea", () => {
+  it("should render the textArea field with label and description", () => {
+    const handleSubmit = jest.fn();
+
+    render(<ComponentTest handleSubmit={handleSubmit} />);
+
+    const labelElement = screen.queryByLabelText(LABEL_TEXT);
+    const descriptionElement = screen.getByText(DESCRIPTION_TEXT);
+    const textAreaElement = screen.getByRole("textbox");
+
+    expect(labelElement).toBeInTheDocument();
+    expect(descriptionElement).toBeInTheDocument();
+    expect(textAreaElement).toBeInTheDocument();
+  });
+
+  it("should handle textArea value change", () => {
+    render(<ComponentTest handleSubmit={() => null} />);
+
+    const textAreaElement = screen.getByLabelText(LABEL_TEXT);
+
+    fireEvent.change(textAreaElement, { target: { value: "John" } });
+
+    expect(textAreaElement).toHaveDisplayValue("John");
+  });
+});
