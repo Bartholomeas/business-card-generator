f6ebfb2158df6441c8d331329d229defba92024a
Bartosz <barth.webdesign@gmail.com>
2024-09-30 10:14:41 +0200

    feat: migrate db from mysql to postgres

--- src/server/api/routers/user/requests/sign-up.ts
+++ src/server/api/routers/user/requests/sign-up.ts
@@ -5,43 +5,54 @@ import { signUpSchema } from "../user.schemas";
 import { publicProcedure } from "~/server/api/trpc";
 
 export const signUp = publicProcedure.input(signUpSchema).mutation(async ({ ctx, input }) => {
-  const { name, email, password, passwordConfirm, policyAgree } = input;
+  try {
+    const { name, email, password, passwordConfirm, policyAgree } = input;
+    console.log("Fifka fifka :))", input);
 
-  const userExists = await ctx.db.user.findUnique({
-    where: { email },
-  });
+    const userExists = await ctx.db.user.findUnique({
+      where: { email },
+    });
+    console.log("Fifka fifka :))", input);
 
-  if (password !== passwordConfirm)
-    throw new TRPCError({
-      code: "CONFLICT",
-      message: "Hasła do siebie nie pasują.",
+    if (password !== passwordConfirm)
+      throw new TRPCError({
+        code: "CONFLICT",
+        message: "Hasła do siebie nie pasują.",
+      });
+
+    if (!policyAgree)
+      throw new TRPCError({
+        code: "CONFLICT",
+        message: "Zgoda jest wymagana.",
+      });
+
+    if (userExists)
+      throw new TRPCError({
+        code: "CONFLICT",
+        message: "Użytkownik o tym adresie e-mail już istnieje.",
+      });
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const result = await ctx.db.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
     });
 
-  if (!policyAgree)
+    return {
+      status: 201,
+      message: "Pomyślnie utworzono konto.",
+      result,
+    };
+  } catch (error) {
+    if (error instanceof TRPCError) {
+      throw error;
+    }
     throw new TRPCError({
-      code: "CONFLICT",
-      message: "Zgoda jest wymagana.",
+      code: "INTERNAL_SERVER_ERROR",
+      message: "An unexpected error occurred",
     });
-
-  if (userExists)
-    throw new TRPCError({
-      code: "CONFLICT",
-      message: "Użytkownik o tym adresie e-mail już istnieje.",
-    });
-
-  const hashedPassword = await bcrypt.hash(password, 12);
-
-  const result = await ctx.db.user.create({
-    data: {
-      name,
-      email,
-      password: hashedPassword,
-    },
-  });
-
-  return {
-    status: 201,
-    message: "Pomyślnie utworzono konto.",
-    result,
-  };
+  }
 });
